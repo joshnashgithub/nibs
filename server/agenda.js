09195fadf2d949aa86bfa1eb44d2c4e5e04808e3
joshnashgithub <34715925+joshnashgithub@users.noreply.github.com>
2018-01-12 17:33:49 -0600

    Create agenda.js

---
+++ server/agenda.js
@@ -0,0 +1,33 @@
+var db = require('./pghelper'),
+    config = require('./config'),
+    winston = require('winston');
+
+function findAll(limit) {
+    return db.query('SELECT id, title, description, image FROM salesforce.Tour_agenda_item__c ORDER BY publishDate DESC LIMIT $2');
+};
+
+function findById(id) {
+    return db.query('SELECT id, title, description, image FROM salesforce.Tour_agenda_item__c');
+};
+
+function getAll(req, res, next) {
+    findAll(20)
+        .then(function (agenda) {
+            return res.send(JSON.stringify(agenda));
+        })
+        .catch(next);
+};
+
+function getById(req, res, next) {
+    var id = req.params.id;
+    findById(id)
+        .then(function (agenda) {
+            return res.send(JSON.stringify(agenda));
+        })
+        .catch(next);
+};
+
+exports.findAll = findAll;
+exports.findById = findById;
+exports.getAll = getAll;
+exports.getById = getById;
