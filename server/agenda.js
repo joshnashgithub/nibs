0704a7af7f65610aa32a7a9c6619a98b27fa2925
joshnashgithub <34715925+joshnashgithub@users.noreply.github.com>
2018-01-13 10:48:31 -0600

    Update agenda.js

--- server/agenda.js
+++ server/agenda.js
@@ -3,7 +3,7 @@ var db = require('./pghelper'),
     winston = require('winston');
 
 function findAll(limit) {
-    return db.query('SELECT Tour_agenda_item__c.id as tour_agenda_id, Tour_agenda_item__c.name as agenda_item_name, Tour_agenda_item__c.description__c as tour_agenda_item_desc, Tour_hotel__c.description__c as tour_hotel_desc, Tour_hotel__c.picture_url__c as tour_hotel_pic FROM Tour_agenda_item__c as tour_agenda_table INNER JOIN Tour_hotel__c as tour_hotel_table ON tour_agenda_table.tour_hotel__c = tour_hotel_table.sfid ORDER BY publishDate DESC LIMIT $2');
+    return db.query('SELECT Tour_agenda_item__c.id as tour_agenda_id, Tour_agenda_item__c.description__c as tour_agenda_item_desc, Tour_hotel__c.description__c as tour_hotel_desc, Tour_hotel__c.picture_url__c as tour_hotel_pic FROM Tour_agenda_item__c as tour_agenda_table INNER JOIN Tour_hotel__c as tour_hotel_table ON tour_agenda_table.tour_hotel__c = tour_hotel_table.sfid ORDER BY publishDate DESC LIMIT $2');
 };
 
 function findById(id) {
