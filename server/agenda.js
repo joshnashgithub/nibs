bb41253510f255c8937438d90693a851dd99c1d6
joshnashgithub <34715925+joshnashgithub@users.noreply.github.com>
2018-01-13 16:23:41 -0600

    Update agenda.js

--- server/agenda.js
+++ server/agenda.js
@@ -6,9 +6,26 @@ function findAll(limit) {
     return db.query('SELECT Tour_agenda_item__c.id as tour_agenda_id, Tour_agenda_item__c.description__c as tour_agenda_item_desc, Tour_hotel__c.description__c as tour_hotel_desc, Tour_hotel__c.picture_url__c as tour_hotel_pic FROM Tour_agenda_item__c as tour_agenda_table INNER JOIN Tour_hotel__c as tour_hotel_table ON tour_agenda_table.tour_hotel__c = tour_hotel_table.sfid ORDER BY publishDate DESC LIMIT $2');
 };
 
-function findById(id) {
+/*function findById(id) {
     return db.query('SELECT Tour_agenda_item__c.id, Tour_agenda_item__c.name, Date_Time__c, description__c, picture_url__c, link_url__c FROM salesforce.Tour_agenda_item__c INNER JOIN salesforce.Tour__c ON salesforce.Tour__c.sfid = salesforce.Tour_agenda_item__c.tour__c INNER JOIN salesforce.Contact ON salesforce.Contact.tour__c = salesforce.Tour__c.sfid  ORDER BY Date_Time__c');
-};
+};*/
+
+
+/**
+ * Get the user's pictures
+ * @param req
+ * @param res
+ * @param next
+ */
+function findById(req, res, next) {
+    var userId = req.userId;
+    db.query("SELECT Tour_agenda_item__c.id, Tour_agenda_item__c.name, Date_Time__c, description__c, picture_url__c, link_url__c FROM salesforce.Tour_agenda_item__c INNER JOIN salesforce.Tour__c ON salesforce.Tour__c.sfid = salesforce.Tour_agenda_item__c.tour__c INNER JOIN salesforce.Contact ON salesforce.Contact.tour__c = salesforce.Tour__c.sfid WHERE userId=$1 ORDER BY Date_Time__c",
+        [userId])
+        .then(function (agenda) {
+            return res.send(JSON.stringify(agenda));
+        })
+        .catch(next);
+}
     
 /*function findById(id) {
     return db.query('SELECT Tour_agenda_item__c.id, Tour_agenda_item__c.name, Tour_agenda_item__c.description__c as agenda_item_desc, Tour_hotel__c.description__c as hotel_desc, Tour_hotel__c.picture_url__c FROM salesforce.Tour_agenda_item__c INNER JOIN salesforce.Tour_hotel__c ON salesforce.Tour_hotel__c.sfid = salesforce.Tour_agenda_item__c.tour_hotel__c');
