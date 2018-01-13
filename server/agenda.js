d0b34ca1572abee3646d9d597c5f9f449f39ea3c
joshnashgithub <34715925+joshnashgithub@users.noreply.github.com>
2018-01-13 16:30:46 -0600

    Update agenda.js

--- server/agenda.js
+++ server/agenda.js
@@ -11,12 +11,11 @@ function findAll(limit) {
 };*/
 
 
-/**
- * Get the user's pictures
- * @param req
- * @param res
- * @param next
- */
+
+ @param req
+ @param res
+ @param next
+
 function findById(req, res, next) {
     var userId = req.userId;
     db.query("SELECT Tour_agenda_item__c.id, Tour_agenda_item__c.name, Date_Time__c, description__c, picture_url__c, link_url__c FROM salesforce.Tour_agenda_item__c INNER JOIN salesforce.Tour__c ON salesforce.Tour__c.sfid = salesforce.Tour_agenda_item__c.tour__c INNER JOIN salesforce.Contact ON salesforce.Contact.tour__c = salesforce.Tour__c.sfid WHERE userId=$1 ORDER BY Date_Time__c",
