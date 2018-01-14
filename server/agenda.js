4ad0c0dbbd548d2e60d2add18b51e448fa64971e
joshnashgithub <34715925+joshnashgithub@users.noreply.github.com>
2018-01-14 11:05:01 -0600

    Update agenda.js

--- server/agenda.js
+++ server/agenda.js
@@ -12,7 +12,7 @@ function getItems(req, res, next) {
     var userId = req.userId;
     console.log("Josh user id is:" + userId);
 
-    db.query("SELECT Tour_agenda_item__c.name, Date_Time__c, description__c, picture_url__c, link_url__c, Contact.id FROM salesforce.Tour_agenda_item__c, salesforce.Contact, salesforce.Tour__c WHERE salesforce.Contact.tour__c = salesforce.Tour_agenda_item__c.tour__c AND salesforce.Contact.id=$1", [userId])
+    db.query("SELECT Tour_agenda_item__c.name, Date_Time__c, description__c, picture_url__c, link_url__c, Contact.id FROM salesforce.Tour_agenda_item__c, salesforce.Contact, salesforce.Tour__c WHERE salesforce.Contact.tour__c = salesforce.Tour_agenda_item__c.tour__c AND salesforce.Contact.id=$1 ORDER BY Date_Time__c", [userId])
         .then(function (agenda) {
             console.log(JSON.stringify(agenda));
             return res.send(JSON.stringify(agenda));
